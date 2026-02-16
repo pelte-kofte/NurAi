e30a9656f722c54c1be02ac4e353d0edc3574989
Bekir Cem Kusdemir <bekircemkusdemir@Bekirs-MacBook-Pro.local>
2026-02-17 02:23:45 +0300

    fix: iOS widget extension embed, build phases and App Store validation fixes

--- ios/NurAiWidgets/NurAiWidgetsBundle.swift
+++ ios/NurAiWidgets/NurAiWidgetsBundle.swift
@@ -1,9 +1,18 @@
+//
+//  NurAiWidgetsBundle.swift
+//  NurAiWidgets
+//
+//  Created by Bekir Cem Kusdemir on 16.02.2026.
+//
+
 import WidgetKit
 import SwiftUI
 
 @main
 struct NurAiWidgetsBundle: WidgetBundle {
-  var body: some Widget {
-    NextPrayerWidget()
-  }
+    var body: some Widget {
+        NurAiWidgets()
+        NurAiWidgetsControl()
+        NurAiWidgetsLiveActivity()
+    }
 }
